912f12bd9cf36aa560834798fdde19da73a194e0
“Sam <sam.alsmadi@gmail.com>
2018-02-24 15:43:19 -0500

    Pass down click function via props

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('clicked')
     this.setState({
       persons: [
-        { name: 'Johnathan', age: 25 },
+        { name: newName, age: 25 },
         { name: 'Mike', age: 28 },
         { name: 'Hana', age: 27 }
       ]
@@ -27,10 +27,17 @@ class App extends Component {
       <div className="App">
         <h1>Hello, there from React!</h1>
         <p>Below is the list of people:</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}  >My Hobbies: Photography</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}  />
+        <button onClick={() => this.switchNameHandler('John!!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'John!')}>My Hobbies: Photography</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}  />
       </div>
     );
   }
