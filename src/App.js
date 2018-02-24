37bf4934aab01e1ff60eb822dee7c51f0ca1fc0b
“Sam <sam.alsmadi@gmail.com>
2018-02-24 15:12:09 -0500

    Add internal state to manage persons

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'John', age: 25 },
+      { name: 'Mike', age: 28 },
+      { name: 'Hana', age:29}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('clicked')
+    this.setState({
+      persons: [
+        { name: 'Johnathan', age: 25 },
+        { name: 'Mike', age: 28 },
+        { name: 'Hana', age: 27 }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello, there from React!</h1>
         <p>Below is the list of people:</p>
-        <Person name="John" age="25"/>
-        <Person name="Mike" age="28" >My Hobbies: Photography</Person>
-        <Person name="Hana" age="29" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}  >My Hobbies: Photography</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}  />
       </div>
     );
   }
