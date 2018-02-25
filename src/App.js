7269a14cf30b06a6fa0b60045e6ac6610fc68210
“Sam <sam.alsmadi@gmail.com>
2018-02-25 14:01:47 -0500

    Toggle persons using external check instead of itinary operator

--- src/App.js
+++ src/App.js
@@ -52,6 +52,24 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
+
+    let persons = null;
+    if(this.state.showPersons){
+      persons = (<div>
+                  <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age} />
+                  <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    changed={this.nameChangedHandler}
+                    click={this.switchNameHandler.bind(this, 'John!')}>My Hobbies: Photography</Person>
+                  <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age} />
+                </div>
+                );
+    }
     return (
       <div className="App">
         <h1>Hello, there from React!</h1>
@@ -59,21 +77,9 @@ class App extends Component {
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          { this.state.showPersons ?
-            <div>
-              <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age} />
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                changed={this.nameChangedHandler}
-                click={this.switchNameHandler.bind(this, 'John!')}>My Hobbies: Photography</Person>
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age} />
-            </div> : null
-          }
+
+          {persons}
+
           <hr />
           <h3>New Tasks</h3>
           <ol>
