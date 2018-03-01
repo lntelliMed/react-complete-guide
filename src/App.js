af94cac31de4d7ede6d08814eb7ddf320bf1c7dc
“Sam <sam.alsmadi@gmail.com>
2018-03-01 15:50:25 -0500

    Add media styling to Person component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from 'radium';
+import Radium, {StyleRoot } from 'radium';
 import Person from './Person/Person';
 import Validation from './Validation/Validation';
 import Char from './Char/Char';
@@ -134,67 +134,69 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hello, there from React!</h1>
-        <p className={classes.join(' ')} >Below is the list of people:</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hello, there from React!</h1>
+          <p className={classes.join(' ')} >Below is the list of people:</p>
+          <button
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
 
-          {persons}
+            {persons}
 
-          <hr />
-        {/* Enter Text: <input type="text" onChange={(event) => this.textLengthHandler(event)} value={this.state.enteredText}/>
-        <br />
-        Entered Text Length: <p>{this.state.enteredText.length}</p>
-        <ValidationComponent enteredTextLength={this.state.enteredText.length} />
-          {enteredChars.map((c, index) =>
-          {
-            return (<CharComponent key={index} enteredChar={c} deleteCharHandler={(event) => {
-               this.deleteCharHandler(event, index);
-            }} />)
+            <hr />
+          {/* Enter Text: <input type="text" onChange={(event) => this.textLengthHandler(event)} value={this.state.enteredText}/>
+          <br />
+          Entered Text Length: <p>{this.state.enteredText.length}</p>
+          <ValidationComponent enteredTextLength={this.state.enteredText.length} />
+            {enteredChars.map((c, index) =>
+            {
+              return (<CharComponent key={index} enteredChar={c} deleteCharHandler={(event) => {
+                this.deleteCharHandler(event, index);
+              }} />)
 
-          })} */}
+            })} */}
 
-          {/* <ol>
-            <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
-            <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
-            <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
-            <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
-            <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
-            <li>When you click a CharComponent, it should be removed from the entered text.</li>
-          </ol>
-          <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p> */}
+            {/* <ol>
+              <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+              <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+              <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
+              <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+              <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
+              <li>When you click a CharComponent, it should be removed from the entered text.</li>
+            </ol>
+            <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p> */}
 
-          <input
-            type="text"
-            onChange={this.inputChangedHandler}
-            value={this.state.userInput} />
-          <p>{this.state.userInput}</p>
-          <Validation inputLength={this.state.userInput.length} />
-          {charList}
+            <input
+              type="text"
+              onChange={this.inputChangedHandler}
+              value={this.state.userInput} />
+            <p>{this.state.userInput}</p>
+            <Validation inputLength={this.state.userInput.length} />
+            {charList}
 
 
-          {/* <h3>New Tasks</h3>
-          <ol>
-            <li>Create TWO new components: UserInput and UserOutput</li>
-            <li>UserInput should hold an input element, UserOutput two paragraphs</li>
-            <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
-            <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
-            <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
-            <li>Add a method to manipulate the state (=> an event-handler method)</li>
-            <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
-            <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
-            <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
-            <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
-          </ol>
-          <UserInput
-            changed={this.usernameChangedHandler}
-            currentName={this.state.username} />
-          <UserOutput userName={this.state.username} />
-          <UserOutput userName={this.state.username} />
-          <UserOutput userName="Josh" /> */}
-      </div>
+            {/* <h3>New Tasks</h3>
+            <ol>
+              <li>Create TWO new components: UserInput and UserOutput</li>
+              <li>UserInput should hold an input element, UserOutput two paragraphs</li>
+              <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
+              <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
+              <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
+              <li>Add a method to manipulate the state (=> an event-handler method)</li>
+              <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
+              <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
+              <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
+              <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
+            </ol>
+            <UserInput
+              changed={this.usernameChangedHandler}
+              currentName={this.state.username} />
+            <UserOutput userName={this.state.username} />
+            <UserOutput userName={this.state.username} />
+            <UserOutput userName="Josh" /> */}
+        </div>
+      </StyleRoot>
     );
   }
 }
