e6d9e782e17a3ad6000113eaec698620ffae5f78
“Sam <sam.alsmadi@gmail.com>
2018-03-05 17:40:06 -0500

    Add update lifecycle hooks caused by state changes to App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,6 +29,19 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount()');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside shouldComponentUpdate()', nextProps, nextState);
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside componentWillUpdate()', nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE App.js] Inside componentDidUpdate()');
+  }
+
   switchNameHandler = (newName) => {
     this.setState({
       persons: [
