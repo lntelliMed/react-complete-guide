e4d565c091d9fdb98e9628eba6f55c7cf9300e5f
“Sam <sam.alsmadi@gmail.com>
2018-03-06 16:11:54 -0500

    Add focus to first input element via ref

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -40,6 +40,7 @@ class Persons extends PureComponent {
         click={() => this.props.clicked(index)}
         name={person.name}
         age={person.age}
+        position={index}
         changed={(event) => { this.props.changed(event, person.id) }}
       />
     });
