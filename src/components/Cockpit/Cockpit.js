f80863a90f948a414f2a1ccc5ac9196ea5f0022b
“Sam <sam.alsmadi@gmail.com>
2018-03-06 02:22:06 -0500

    Use Aux hoc to wrap Cockpit component instead of div element

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Aux from '../../hoc/Aux';
 
 const cockpit = (props) => {
-  let btnClass = '';
+  let btnClass = classes.Button;
   if(props.showPersons){
-    btnClass = classes.Red;
+    btnClass = [classes.Button, classes.Red].join( ' ' );
   }
   const assignedClasses = [];
   if (props.persons.length <= 2) {
@@ -15,13 +16,13 @@ const cockpit = (props) => {
     assignedClasses.push(classes.bold);
   }
   return (
-    <div className={classes.Cockpit}>
+    <Aux>
       <h1>{props.appTitle}</h1>
       <p className={assignedClasses.join(' ')} >Below is the list of people:</p>
       <button
         className={btnClass}
         onClick={props.clicked}>Toggle Persons</button>
-      </div>
+    </Aux>
   );
 }
 
