7adfe2369fd55263755d1be13b05caed89d9cb01
“Sam <sam.alsmadi@gmail.com>
2018-04-12 03:26:52 -0400

    refact: split root reducer into two

--- 03-react-redux/src/containers/Counter/Counter.js
+++ 03-react-redux/src/containers/Counter/Counter.js
@@ -36,7 +36,7 @@ class Counter extends Component {
                 <CounterControl label="Add 5" clicked={() => this.props.onAddCounter(5)}  />
                 <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter(5)}  />
                 <hr />
-                <button onClick={this.props.onStoreResult} >Store Result</button>
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(result => {
                         return <li key={result.id} onClick={() => this.props.onDeleteResult(result.id) } >{result.value}</li>
@@ -50,8 +50,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter,
-        storedResults: state.results
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
     };
 };
 
@@ -61,7 +61,7 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
         onAddCounter: (value) => dispatch({ type: actionTypes.ADD, value }),
         onSubtractCounter: (value) => dispatch({ type: actionTypes.SUBTRACT, value }),
-        onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
+        onStoreResult: (result) => dispatch({ type: actionTypes.STORE_RESULT, result }),
         onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, resultId: id})
     };
 };
