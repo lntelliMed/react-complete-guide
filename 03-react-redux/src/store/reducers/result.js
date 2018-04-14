a1b0edb829d1f5a7db8355b962807be4beb629b9
“Sam <sam.alsmadi@gmail.com>
2018-04-14 01:56:04 -0400

    refact: use separate function for deleteResult case

--- 03-react-redux/src/store/reducers/result.js
+++ 03-react-redux/src/store/reducers/result.js
@@ -5,27 +5,14 @@ const initialState = {
   results: []
 }
 
+const deleteResult = (state, action) => {
+  return updateObject(state, { results: state.results.filter(result => result.id !== action.resultId) });
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.STORE_RESULT:
-      // return {
-      //   ...state,
-      //   results: state.results.concat({ id: new Date(), value: action.result })
-      // }
-      return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result })});
-    case actionTypes.DELETE_RESULT:
-      // let id = 2;
-      // let resultsCopy = [...state.results];
-      // resultsCopy.splice(2, 1);
-      // return {
-      //     ...state,
-      //     results: resultsCopy
-      // }
-      // return {
-      //   ...state,
-      //   results: state.results.filter(result => result.id !== action.resultId)
-      // }
-      return updateObject(state, { results: state.results.filter(result => result.id !== action.resultId) });
+    case actionTypes.STORE_RESULT: return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result })});
+    case actionTypes.DELETE_RESULT: return deleteResult(state, action);
   }
   return state;
 };
