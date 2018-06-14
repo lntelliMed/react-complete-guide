1b91e67a2a021a45ab407c542e30006480c88c2c
“Sam <sam.alsmadi@gmail.com>
2018-06-13 23:32:57 -0400

    Add code splitting example

---
+++ 11-code-splitting-example/src/AsyncComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+const asyncComponent = (importComponent) => {
+  return class AsyncComponent extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        component: null
+      };
+    }
+
+    async componentDidMount() {
+      const { default: component } = await importComponent();
+
+      this.setState({
+        component
+      });
+    }
+
+    render() {
+      const Component = this.state.component;
+      return Component ? <Component {...this.props} /> : null;
+    }
+  }
+}
+
+export default asyncComponent;
